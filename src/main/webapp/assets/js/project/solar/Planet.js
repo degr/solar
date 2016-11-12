f60c4993d0baa5a705fc1713a441dc81c3033a66
degradator@bk.ru <QWErty123>
2016-11-13 00:04:20 +0300

    planet orbit render + satellite render

--- src/main/webapp/assets/js/project/solar/Planet.js
+++ src/main/webapp/assets/js/project/solar/Planet.js
@@ -24,24 +24,36 @@ Engine.define('Planet', ['SolarSystem', 'Profile', 'CanvasClickProxy'], function
     }
 
     Planet.prototype.draw = function(context, zoomWindow, locations) {
-        var size = Math.min(context.canvas.width, context.canvas.height);
-        var planetInfo = Planet.drawPlanetoid(this, context, zoomWindow,
+        var planetInfo = Planet.drawPlanetoid(
+            this,
+            context,
+            zoomWindow,
             locations,
-            {x: size / 2, y: size / 2},
-            {orbit: 'steelblue', planetoid: '#01DF3A', label: '#00FF40'}
+            {orbit: 'steelblue', planetoid: '#01DF3A', label: '#00FF40'},
+            SolarSystem.radius,
+            SolarSystem.radius
         );
 
 
-        if(planetInfo.radius > 2 && this.satellites && this.satellites.length) {
-            for(var i = 0; i < this.satellites.length; i++) {
-                planetInfo = Planet.drawPlanetoid(
-                    this.satellites[i],
-                    context,
-                    zoomWindow,
-                    locations,
-                    {x:planetInfo.x, y: planetInfo.y},
-                    {orbit: '#FAAC58', planetoid: '#F3E2A9', label: '#F3E2A9'}
-                );
+        if(this.satellites && this.satellites.length) {
+            if (planetInfo.radius > 2) {
+                var shiftX = planetInfo.x / zoomWindow.ratio + zoomWindow.rectangle.x;
+                var shiftY = planetInfo.y / zoomWindow.ratio + zoomWindow.rectangle.y;
+                for (var i = 0; i < this.satellites.length; i++) {
+                    Planet.drawPlanetoid(
+                        this.satellites[i],
+                        context,
+                        zoomWindow,
+                        locations,
+                        {orbit: '#FAAC58', planetoid: '#F3E2A9', label: '#F3E2A9'},
+                        shiftX,
+                        shiftY
+                    );
+                }
+            } else {
+                for (var j = 0; j < this.satellites.length; j++) {
+                    Planet.updateAngle(this.satellites[j]);
+                }
             }
         }
     };
@@ -55,25 +67,47 @@ Engine.define('Planet', ['SolarSystem', 'Profile', 'CanvasClickProxy'], function
     };
 
 
-    Planet.drawPlanetoid = function(planetoid, context, zoomWindow, locations, center, colors) {
+    Planet.updateAngle = function(planetoid) {
         if(planetoid.speed) {
             if(planetoid.backMove) {
                 planetoid.angle -= Math.PI / (planetoid.speed * Profile.speed);
+                console.log(planetoid.name, planetoid.angle);
             } else {
+                if(planetoid.name.indexOf('lun') == 0) {
+                    console.log(planetoid.name, planetoid.angle);
+                }
                 planetoid.angle += Math.PI / (planetoid.speed * Profile.speed);
             }
         }
+    };
+
+    Planet.drawPlanetoid = function(planetoid, context, zoomWindow, locations, colors, shiftX, shiftY) {
+        Planet.updateAngle(planetoid);
         var orbitRadius = planetoid.orbit;
         var planetRadius = planetoid.radius * zoomWindow.ratio;
-        var x = ((Math.cos(planetoid.angle) * orbitRadius + SolarSystem.radius) - zoomWindow.rectangle.x) * zoomWindow.ratio;
-        var y = ((Math.sin(planetoid.angle) * orbitRadius + SolarSystem.radius) - zoomWindow.rectangle.y) * zoomWindow.ratio;
+        var x = ((Math.cos(planetoid.angle) * orbitRadius + shiftX) - zoomWindow.rectangle.x) * zoomWindow.ratio;
+        var y = ((Math.sin(planetoid.angle) * orbitRadius + shiftY) - zoomWindow.rectangle.y) * zoomWindow.ratio;
         context.beginPath();
         context.strokeStyle = colors.orbit;
-        /*if(planetoid.backMove) {
-            context.arc(center.x, center.y, orbitRadius,  planetoid.angle, planetoid.angle + Math.PI / 4, false);
+        if(planetoid.backMove) {
+            context.arc(
+                (shiftX - zoomWindow.rectangle.x) * zoomWindow.ratio,
+                (shiftY  - zoomWindow.rectangle.y)* zoomWindow.ratio,
+                orbitRadius* zoomWindow.ratio,
+                planetoid.angle ,
+                planetoid.angle + Math.PI / 4,
+                false
+            );
         } else {
-            context.arc(center.x, center.y, orbitRadius, planetoid.angle, planetoid.angle - Math.PI / 4, true);
-        }*/
+            context.arc(
+                (shiftX - zoomWindow.rectangle.x) * zoomWindow.ratio,
+                (shiftY  - zoomWindow.rectangle.y)* zoomWindow.ratio,
+                orbitRadius* zoomWindow.ratio,
+                planetoid.angle,
+                planetoid.angle - Math.PI / 4,
+                true
+            );
+        }
         context.stroke();
 
         context.strokeStyle = colors.planetoid;
@@ -86,9 +120,9 @@ Engine.define('Planet', ['SolarSystem', 'Profile', 'CanvasClickProxy'], function
             context.drawImage(planetoid.image, x - pseudoRadius, y - pseudoRadius, pseudoRadius * 2, pseudoRadius * 2 );
         }
 
-        /*context.textAlign = 'center';
+        context.textAlign = 'center';
         context.strokeStyle = colors.label;
-        context.strokeText(planetoid.name, x, y);*/
+        context.strokeText(planetoid.name, x, y);
         locations.push(new CanvasClickProxy(x, y, planetRadius < 5 ? 5 : planetRadius, function(params){
             console.log(planetoid);
         }));
