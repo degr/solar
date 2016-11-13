92e5b07fa44464828969eaa00e7e1f909c730eb1
degradator@bk.ru <QWErty123>
2016-11-13 19:27:05 +0300

    ship popup initial

--- src/main/webapp/assets/js/project/components/popups/ShipPopup.js
+++ src/main/webapp/assets/js/project/components/popups/ShipPopup.js
@@ -5,6 +5,8 @@ Engine.define('ShipPopup', ['Popup', 'Dom'], function() {
         var popup = new Popup({title: 'Ship', isOpen: true, withOverlay: false, controlClose: false});
         this.popup = popup;
         this.container = popup.container;
+        this.container.style.left = 0;
+        this.container.style.top = 0;
     }
 
     ShipPopup.prototype.update = function(params) {
